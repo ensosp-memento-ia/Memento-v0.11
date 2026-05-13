e743637e80b9e12d21746e9b4b5df769f0fcba4a
Mémento IA - ENSOSP <nanou.lefevrec@gmail.com>
2026-05-13 11:23:59 +0200

    Create config.js

---
+++ src/ui/config.js
@@ -0,0 +1,2 @@
+// Constantes globales partagées entre les modules
+export const BETA_FORM_URL = "https://forms.office.com/Pages/ResponsePage.aspx?id=8fedXl6ZuESKAGhF_Bb8M5J2aSnQSghAnRmJ9DwIhUxUOFA1Q0lOT0FCSUU4TDU3WklSTTVGRzlMMy4u";
